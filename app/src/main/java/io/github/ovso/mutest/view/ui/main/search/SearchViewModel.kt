7615315f63c60847d3d6f67c24cd9387310c3ae4
jaeho <ovsoce@gmail.com>
2019-05-22 17:39:39 +0900

    EditText databinding

--- app/src/main/java/io/github/ovso/mutest/view/ui/main/search/SearchViewModel.kt
+++ app/src/main/java/io/github/ovso/mutest/view/ui/main/search/SearchViewModel.kt
@@ -1,31 +1,14 @@
 package io.github.ovso.mutest.view.ui.main.search
 
-import android.text.Editable
-import android.text.TextWatcher
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
 class SearchViewModel : ViewModel() {
-  fun doSearch() {
-    Timber.d("doSearch")
-  }
+  val textChangeObField = ObservableField<String>()
 
-  fun onTextChanged(s: CharSequence, start: Int, count: Int) {
-    Timber.d("onTextChanged($s, $start, $count)")
-  }
-
-  val onTextWatcher = object : TextWatcher {
-    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-      Timber.d("doSearch")
-    }
-
-    override fun afterTextChanged(s: Editable?) {
-      Timber.d("doSearch")
-    }
-
-    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-      Timber.d("doSearch")
-    }
+  fun onSearchClick() {
+    Timber.d("doSearch = ${textChangeObField.get()}")
   }
 
 }
